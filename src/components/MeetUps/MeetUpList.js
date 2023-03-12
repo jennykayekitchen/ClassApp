c30ec090888e28c27273602b58c193920e84bee8
Jenny Kitchen <jennykayekitchen@gmail.com>
2023-03-12 13:02:01 -0500

    view all meetups

--- src/components/MeetUps/MeetUpList.js
+++ src/components/MeetUps/MeetUpList.js
@@ -3,9 +3,35 @@ import { MeetUp } from "./MeetUp"
 
 
 export const MeetUpList = () => {
-       
+    const [meetups, setMeetups] = useState([])
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/meetups`)
+                .then(response => response.json())
+                .then((data) => {
+                    setMeetups(data)
+                    
+                })                
+        },
+        []
+    )
     //renders the list of all meet-ups
     return <>
-        <h2>All Meet Ups</h2>    
+        <em className="songlist_tape"></em>
+                <div>{meetups.map(meetup => <MeetUp key={`meetup--${meetup.id}`} 
+                meetupId={meetup.id}
+                meetupTitle={meetup.title}
+                meetupType={meetup.type}
+                meetupVenue={meetup.venue}
+                meetupAddress={meetup.address}
+                meetupLink={meetup.link}
+                meetupNeighborhood={meetup.neighborhood}
+                meetupDate={meetup.date}
+                meetupTime={meetup.time}
+                meetupDescription={meetup.description}
+                />
+                )}         
+                        
+                </div> 
     </>
 }
